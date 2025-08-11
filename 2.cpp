1d7e1fc2f9bc21f0e836052eb0080f163f44aab0
rishitajain7 <rjain1_be24@thapar.edu>
2025-08-11 20:32:05 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[50];
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter " << n << " elements: "<<endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; ) {
+            if (arr[i] == arr[j]) {
+                for (int k = j; k < n - 1; k++) {
+                    arr[k] = arr[k + 1];}
+                n--; } 
+                else { j++;}
+        } }
+
+    cout << "Array after removing duplicates:"<<endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
