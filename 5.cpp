2a724b3cc20142805ecfeaafe5bfd78a4e6f5270
rishitajain7 <rjain1_be24@thapar.edu>
2025-08-11 20:33:41 +0530

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10][10];
+    int rows, cols;
+
+    cout << "Enter number of rows and columns: ";
+    cin >> rows >> cols;
+
+    cout << "Enter elements of the matrix: "<<endl
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    // Sum of each row
+    cout << "\nSum of each row:\n";
+    for (int i = 0; i < rows; i++) {
+        int rowSum = 0;
+        for (int j = 0; j < cols; j++) {
+            rowSum += a[i][j];
+        }
+        cout << "Row " << i + 1 << ": " << rowSum << endl;
+    }
+
+    // Sum of each column
+    cout << "\nSum of each column:\n";
+    for (int j = 0; j < cols; j++) {
+        int colSum = 0;
+        for (int i = 0; i < rows; i++) {
+            colSum += a[i][j];
+        }
+        cout << "Column " << j + 1 << ": " << colSum << endl;
+    }
+
+    return 0;
+}
